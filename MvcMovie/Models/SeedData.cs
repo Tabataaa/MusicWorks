b4d384130902218d9432d1f18f2544173f5237e2
Tabataaa <92487673+Tabataaa@users.noreply.github.com>
2021-10-23 11:53:10 -0400

    Moving code to git

---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Are You Lonesome Tonight?",
+                        ReleaseDate = DateTime.Parse("1960"),
+                        Genre = "Pop",
+                        Rating = "null",
+                        Price = 7.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "I Want to Hold Your Hand",
+                        ReleaseDate = DateTime.Parse("1964"),
+                        Genre = "Pop",
+                        Rating = "null",
+                        Price = 8.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ghostbusters 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Rating = "PG-13",
+                        Price = 9.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Rating = "PG",
+                        Price = 3.99M
+                    }
+                   
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
